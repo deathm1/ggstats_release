1bcd763792600a5fdaac579bc9ae2a5657a264e6
deathm1 <k0shur98@outlook.com>
2022-09-18 17:48:33 +0530

    client_dep v1.0.0

--- src/Pages/ShortLinkHandler/ShortLinkHandler.js
+++ src/Pages/ShortLinkHandler/ShortLinkHandler.js
@@ -1,16 +1,19 @@
 import * as React from 'react'
+import { useParams } from 'react-router-dom'
 
 export default function ShortLinkHandler(props) {
   const [url, seturl] = React.useState('asdasdasd')
+
+  const { handle } = useParams()
 
   React.useEffect(() => {
     return () => {
       const raw = window.location.pathname
       if (raw !== '/') {
-        const url = raw
+        const url = handle
         seturl(url)
       }
     }
-  }, [])
+  }, [handle])
   return <>{url}</>
 }
